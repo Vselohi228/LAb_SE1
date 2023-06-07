94f75dc9a6be3d201db08b6b6b96384919efd5e3
Vselohi228 <128789629+Vselohi228@users.noreply.github.com>
2023-06-07 21:55:25 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, y, sum, dif;
+
+    cout << "Enter x: " << endl; cin >> x;
+    cout << "Enter y: " << endl; cin >> y;
+    cout << "Sum: " << x + y << endl;
+    cout << "Difference: " << x - y << endl;
+    cout << "x * y = " << x * y << endl;
+    cout << "x / y = " << x / y << endl;
+    return 0;
+}
